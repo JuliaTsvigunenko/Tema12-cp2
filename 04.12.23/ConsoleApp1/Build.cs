f95c53e9b6378e8ec71b81c9be8099002a71e7c7
JuliaTsvigunenko <122933386+JuliaTsvigunenko@users.noreply.github.com>
2023-12-04 16:09:52 +0200

    Add files via upload

---
+++ 04.12.23/ConsoleApp1/Build.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Класс описывающий строение
+    /// </summary>
+    class Build
+    {
+
+        /// <summary>
+        /// Создание поля имени
+        /// </summary>
+        public string name;
+        /// <summary>
+        /// Создание поля площади
+        /// </summary>
+        public double area;
+        /// <summary>
+        /// Создание поля кол-ва жильцов
+        /// </summary>
+        public int kvo;
+        /// <summary>
+        /// Создания поля этажей
+        /// </summary>
+        public int floor;
+
+
+        /// <summary>
+        /// конструктор по умолчанию
+        /// </summary>
+        public Build()
+        {
+        }
+        /// <summary>
+        /// конструктор с параметрами 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="area"></param>
+        /// <param name="kvo"></param>
+
+        public Build(string name, double area, int kvo)
+
+        {
+            this.name = name;
+            this.area = area;
+            this.kvo = kvo;
+        }
+
+
+        public Build(string name, double area, int kvo, int floor):this(name, area, kvo)
+        {
+
+        
+            this.floor = floor;
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Имя - {name}\nПлощадь - {area}\nКол-во жильцов - {kvo}\nПлощадь на одного жильца - {area/kvo}\nКол-во этажей - {floor}";
+        }
+
+
+
+
+
+
+    }
+}
---
+++ 04.12.23/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Build dom1 = new Build();
+            dom1.name = "Юность";
+            dom1.area = 300;
+            dom1.kvo = 2;
+            Console.WriteLine(dom1.ToString());
+            
+            Build dom2 = new Build("Дача",150,4);
+            Console.WriteLine(dom2.ToString());
+
+            /// <summary>
+            /// Способ, где параметры вводятся с клавиатуры 
+            /// </summary>
+            Console.WriteLine("name: ");
+            string name =Console.ReadLine();
+            Console.WriteLine("area: ");
+            double area =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("kvo: ");
+            int kvo =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("floor: ");
+            int floor =Convert.ToInt32(Console.ReadLine());
+
+            Build dom3 = new Build(name,area, kvo, floor);
+            Console.WriteLine($"{dom3.ToString()}");
+
+            Console.ReadKey();
+        }
+    }
+}
